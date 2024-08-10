f015a002fd1e0fabf3aad122d09226ca423b19f8
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-08-10 12:24:12 +0100

    ft: add base loop logic

--- text-guesser/src/main.rs
+++ text-guesser/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut total_runs: u32 = 0;
+    let mut correct_guesses: u32 = 0;
+    let mut loop_guard = true;
+
+    while loop_guard {
+        total_runs += 1;
+        println!("Guess an integer between 0 and 20. If you guess correctly, you get a point:");
+
+        let mut input = String::new();
+        let n = stdin().read_line(&mut input).unwrap();
+        println!("Bytes read {}", n);
+        println!("Input read {}", input);
+
+        if input == "exit" {
+            println!("Out of {} runs, you guessed {} successfully", total_runs, correct_guesses);
+            loop_guard = false;
+        } else {
+            print!("Hello");
+        }
+    }
 }
