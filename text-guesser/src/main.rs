36a57cac5539677cb86e4d028d2f1c28137eb471
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2024-08-10 16:40:59 +0100

    ft: fix entry parsing for the text guesser and add error handling

--- text-guesser/src/main.rs
+++ text-guesser/src/main.rs
@@ -1,24 +1,50 @@
 use std::io::stdin;
+use rand::{Rng};
 
 fn main() {
     let mut total_runs: u32 = 0;
     let mut correct_guesses: u32 = 0;
     let mut loop_guard = true;
+    let mut rng = rand::thread_rng();
+    let max_range = 10;
+    
+    println!();
+    println!("WELCOME TO TEXT GUESSR");
 
     while loop_guard {
-        total_runs += 1;
-        println!("Guess an integer between 0 and 20. If you guess correctly, you get a point:");
+        println!();
+        println!("Guess an integer between 0 and {}. If you guess correctly, you get a point.", max_range);
+        println!("If you are done playing, type 'exit' and get your results.");
+        println!();
 
         let mut input = String::new();
-        let n = stdin().read_line(&mut input).unwrap();
-        println!("Bytes read {}", n);
-        println!("Input read {}", input);
+        stdin().read_line(&mut input).unwrap();
+        let sanitized_input = input.trim();
 
-        if input == "exit" {
-            println!("Out of {} runs, you guessed {} successfully", total_runs, correct_guesses);
+        if sanitized_input == "exit" {
+            println!("Out of {} runs, you guessed {} successfully.", total_runs, correct_guesses);
             loop_guard = false;
         } else {
-            print!("Hello");
+            let parse_result = sanitized_input.parse::<i32>();
+            
+            match parse_result { 
+                Ok(value) => {
+                    if value < 0 || value > max_range {
+                        println!("Invalid value");
+                    } else { 
+                        let random: i32 = rng.gen_range(0..max_range + 1);
+                        total_runs += 1;
+                        
+                        if random == value {
+                            correct_guesses += 1;
+                            println!("Wow, you are good at this. You guessed right");
+                        } else { 
+                            println!("I guess you aren't good at this. The correct value is {}", random);
+                        }
+                    } 
+                },
+                Err(_error) => println!("Invalid value")
+            }
         }
     }
 }
