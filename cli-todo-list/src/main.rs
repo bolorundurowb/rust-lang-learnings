d142197dddf41aec7b73de31c7e1527f713eef35
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-08-11 23:06:03 +0100

    ft: add clean and fix the todo add logic

--- cli-todo-list/src/main.rs
+++ cli-todo-list/src/main.rs
@@ -1,19 +1,32 @@
 use std::env::args;
-use std::fs::File;
-use std::io::Read;
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
 use std::path::Path;
 use inline_colorization::{color_green, color_red, color_reset, color_cyan, color_yellow};
 
 const DB_PATH: &str = "./todo.db";
 
 fn main() {
-    print_header();
     let command_opt = args().nth(1);
 
     match command_opt {
         Some(command) => {
             match command.as_str() {
                 "list" => { list_todos(); }
+                "add" => {
+                    let todo_opt = args().nth(2);
+
+                    match todo_opt {
+                        Some(todo_entry) => add_todo(todo_entry.as_str()),
+                        None => {
+                            println!("{color_red}Arguments are required for the 'add' command{color_reset}");
+                            println!();
+                            print_header();
+                        }
+                    }
+                }
+                "clean" => { clean_todos(); }
                 "-h" | "--help" => { print_header(); }
                 _ => {
                     println!("{color_red}Unknown command '{}' provided{color_reset}", command);
@@ -30,10 +43,26 @@ fn main() {
     }
 }
 
+fn add_todo(input: &str) {
+    ensure_db();
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(DB_PATH)
+        .unwrap();
+    writeln!(file, "{}", input).expect("Write failed");
+    println!("{color_green}Todo written successfully{color_reset}");
+}
+
+fn clean_todos() {
+    fs::remove_file(DB_PATH).expect("Failed to clean out todos");
+}
+
 fn list_todos() {
     ensure_db();
 
-    let mut file =  File::open(DB_PATH).expect("Failed to read database file");
+    let mut file = File::open(DB_PATH).expect("Failed to read database file");
     let mut content = String::new();
 
     file.read_to_string(&mut content).expect("Failed to read database file");
@@ -68,9 +97,10 @@ fn print_header() {
     println!("{color_green}Usage:{color_reset} {color_cyan}cli-todo-list [command] [args]{color_reset}");
     println!();
     println!("{color_green}Commands:{color_reset}");
-    println!("        {color_cyan}list{color_reset}           Print all embedded todos");
-    println!("        {color_cyan}add{color_reset}            Add a new todo list item");
-    println!("        {color_cyan}rm{color_reset}             Remove a todo item");
-    println!("    {color_cyan}-h, --help{color_reset}         Print help");
+    println!("     {color_cyan}list{color_reset}           Print all embedded todos");
+    println!("     {color_cyan}add{color_reset}            Add a new todo list item");
+    println!("     {color_cyan}rm{color_reset}             Remove a todo item");
+    println!("     {color_red}clean{color_reset}          Clear all todos");
+    println!(" {color_cyan}-h, --help{color_reset}         Print help");
     println!();
 }
