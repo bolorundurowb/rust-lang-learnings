4c5779bf6e9aff2ab16c1349fd7e578909e4a8a3
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-09-02 22:47:59 +0100

    ft: improve the tokenizer code

--- cli-calculator/src/main.rs
+++ cli-calculator/src/main.rs
@@ -2,6 +2,9 @@ use std::env::args;
 use inline_colorization::{color_red, color_reset};
 use unicode_segmentation::UnicodeSegmentation;
 
+const NORMAL_DECIMAL_DELINEATOR: char = '.';
+const WEIRD_EURO_DECIMAL_DELINEATOR: char = ',';
+
 #[derive(Debug, Clone)]
 struct ParseError(String);
 
@@ -56,55 +59,55 @@ fn main() {
 }
 
 fn tokenize(input: String) -> Vec<Token> {
+    let input_length = input.graphemes(true).count() as i32;
     let mut tokens: Vec<Token> = vec!();
     let mut index: i32 = 0;
     let mut raw_value: String = String::new();
 
     for letter in input.chars() {
-        if (letter.is_digit(10) || letter == '.' || letter == ',') {
+        if letter.is_digit(10) || letter == NORMAL_DECIMAL_DELINEATOR || letter == WEIRD_EURO_DECIMAL_DELINEATOR {
             raw_value = format!("{}{}", raw_value, letter);
-        } else if letter.is_whitespace() {
-            if !raw_value.is_empty() {
-                tokens.push(Token::Operand {
-                    end_index: index,
-                    start_index: index - raw_value.graphemes(true).count() as i32,
-                    raw_value: raw_value.clone(),
-                });
-                // reset the value read out
-                raw_value = String::new();
-            }
-        } else if index == input.graphemes(true).count() as i32 {
-            if !raw_value.is_empty() {
-                tokens.push(Token::Operand {
-                    end_index: index,
-                    start_index: index - raw_value.graphemes(true).count() as i32,
-                    raw_value: raw_value.clone(),
-                });
-                // reset the value read out
-                raw_value = String::new();
-            }
-        }  else {
+        } else if letter.is_whitespace() || index == input_length {
+            append_if_operand(&mut tokens, index, &mut raw_value);
+        } else {
             // stash the parsed operand
-            if !raw_value.is_empty() {
-                tokens.push(Token::Operand {
-                    end_index: index,
-                    start_index: index - raw_value.graphemes(true).count() as i32,
-                    raw_value: raw_value.clone(),
-                });
-                // reset the value read out
-                raw_value = String::new();
-            }
+            append_if_operand(&mut tokens, index, &mut raw_value);
 
             // this should be an operator
             let operator = Operator::parse(letter).unwrap();
             tokens.push(Token::Operator {
                 index,
-                operator
+                operator,
             });
         }
 
         index += 1;
     }
-    
-    return tokens;
+
+    tokens
+}
+
+fn append_if_operand(mut tokens: &mut Vec<Token>, index: i32, raw_value: &mut String) {
+    let token_opt = evaluate_operand(index, &raw_value);
+    match token_opt {
+        Some(token) => {
+            tokens.push(token);
+            raw_value.clear();
+        }
+        None => {}
+    }
+}
+
+fn evaluate_operand(index: i32, raw_value: &String) -> Option<Token> {
+    if !raw_value.is_empty() {
+        let token = Token::Operand {
+            end_index: index,
+            start_index: index - raw_value.graphemes(true).count() as i32,
+            raw_value: raw_value.clone(),
+        };
+
+        return Some(token);
+    }
+
+    None
 }
