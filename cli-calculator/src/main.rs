3d269b5eafa70b0811c55179869d84fda54747ce
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-12-07 23:26:17 +0100

    ft: finalize evaluation logic ahead of testing

--- cli-calculator/src/main.rs
+++ cli-calculator/src/main.rs
@@ -1,33 +1,20 @@
+use inline_colorization::{color_red, color_reset};
 use std::cmp::PartialEq;
 use std::env::args;
-use inline_colorization::{color_red, color_reset};
 use unicode_segmentation::UnicodeSegmentation;
 
 const NORMAL_DECIMAL_DELINEATOR: char = '.';
 const WEIRD_EURO_DECIMAL_DELINEATOR: char = ',';
 
-#[derive(Debug, Clone)]
-struct ParseError(String);
+#[derive(Debug)]
+struct ParseError();
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Operator {
     Add,
     Subtract,
     Multiply,
     Divide,
-}
-
-#[derive(Debug)]
-enum Token {
-    Operator {
-        index: i32,
-        operator: Operator,
-    },
-    Operand {
-        start_index: i32,
-        end_index: i32,
-        raw_value: String,
-    },
 }
 
 impl Operator {
@@ -39,23 +26,34 @@ impl Operator {
             '/' => Ok(Operator::Divide),
             '+' => Ok(Operator::Add),
             '-' => Ok(Operator::Subtract),
-            _ => Err(ParseError(format!("Invalid operator provided: '{}'", value)))
+            _ => Err(ParseError()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Token {
+    Operator { operator: Operator },
+    Operand { parsed_value: f32 },
+}
+
+impl Token {
+    fn to_operand(&self) -> Operand {
+        match self {
+            Token::Operator { .. } => {
+                panic!();
+            }
+            Token::Operand { parsed_value, .. } => Operand(*parsed_value),
         }
     }
 }
 
 #[derive(Debug)]
-enum Operand {
-    Value(f32),
-    Operation(dyn Operation),
-}
+struct Operand(f32);
 
 impl Operand {
     fn to_value(&self) -> f32 {
-        match &self {
-            Operand::Value(value) => value.to_owned(),
-            Operand::Operation(operation) => operation.evaluate()
-        }
+        self.0
     }
 }
 
@@ -83,7 +81,7 @@ impl Operation for UnaryOperation {
         match self.operator {
             Operator::Add => evaluated_operand,
             Operator::Subtract => 0f32 - evaluated_operand,
-            _ => panic!("Unsupported unary iperator")
+            _ => panic!("Unsupported unary operator"),
         }
     }
 }
@@ -102,26 +100,20 @@ impl Operation for BinaryOperation {
     }
 }
 
-impl PartialEq<Operator> for &Operator {
-    fn eq(&self, other: &Operator) -> bool {
-        *self == *other
-    }
-}
-
 fn main() {
     let expression_opt = args().nth(1);
 
     match expression_opt {
         Some(expression) => {
-            let tokens: Vec<Token> = tokenize(expression);
+            let tokens: Vec<Token> = tokenize(&expression);
             let is_valid = validate_tokens(&tokens);
 
             if !is_valid {
                 panic!("Invalid expression: {}", expression);
             }
 
-            let operation = to_operation(tokens);
-            println!("The result of is {:?}", operation.evaluate());
+            let result = evaluate(tokens);
+            println!("The result of is {:?}", result);
         }
         None => {
             println!("{color_red}No expression provided{color_reset}");
@@ -130,27 +122,27 @@ fn main() {
     }
 }
 
-fn tokenize(input: String) -> Vec<Token> {
+fn tokenize(input: &String) -> Vec<Token> {
     let input_length = input.graphemes(true).count() as i32;
-    let mut tokens: Vec<Token> = vec!();
+    let mut tokens: Vec<Token> = vec![];
     let mut index: i32 = 0;
     let mut raw_value: String = String::new();
 
     for letter in input.chars() {
-        if letter.is_digit(10) || letter == NORMAL_DECIMAL_DELINEATOR || letter == WEIRD_EURO_DECIMAL_DELINEATOR {
+        if letter.is_digit(10)
+            || letter == NORMAL_DECIMAL_DELINEATOR
+            || letter == WEIRD_EURO_DECIMAL_DELINEATOR
+        {
             raw_value = format!("{}{}", raw_value, letter);
         } else if letter.is_whitespace() || index == input_length {
-            append_if_operand(&mut tokens, index, &mut raw_value);
+            append_if_operand(&mut tokens, &mut raw_value);
         } else {
             // stash the parsed operand
-            append_if_operand(&mut tokens, index, &mut raw_value);
+            append_if_operand(&mut tokens, &mut raw_value);
 
             // this should be an operator
             let operator = Operator::parse(letter).unwrap();
-            tokens.push(Token::Operator {
-                index,
-                operator,
-            });
+            tokens.push(Token::Operator { operator });
         }
 
         index += 1;
@@ -159,8 +151,8 @@ fn tokenize(input: String) -> Vec<Token> {
     tokens
 }
 
-fn append_if_operand(mut tokens: &mut Vec<Token>, index: i32, raw_value: &mut String) {
-    let token_opt = evaluate_operand(index, &raw_value);
+fn append_if_operand(tokens: &mut Vec<Token>, raw_value: &mut String) {
+    let token_opt = evaluate_operand(&raw_value);
     match token_opt {
         Some(token) => {
             tokens.push(token);
@@ -170,12 +162,10 @@ fn append_if_operand(mut tokens: &mut Vec<Token>, index: i32, raw_value: &mut St
     }
 }
 
-fn evaluate_operand(index: i32, raw_value: &String) -> Option<Token> {
+fn evaluate_operand(raw_value: &String) -> Option<Token> {
     if !raw_value.is_empty() {
         let token = Token::Operand {
-            end_index: index,
-            start_index: index - raw_value.graphemes(true).count() as i32,
-            raw_value: raw_value.clone(),
+            parsed_value: normalize_numeric_string(raw_value).parse::<f32>().unwrap(),
         };
 
         return Some(token);
@@ -184,45 +174,48 @@ fn evaluate_operand(index: i32, raw_value: &String) -> Option<Token> {
     None
 }
 
-fn normalize_numeric_string(mut value: &String) -> String {
-    value.chars()
+fn normalize_numeric_string(value: &String) -> String {
+    value
+        .chars()
         .filter(|&c| c != '.' && c != ' ')
         .map(|c| if c == ',' { '.' } else { c })
         .collect::<String>()
 }
 
 fn validate_tokens(tokens: &Vec<Token>) -> bool {
-    let last_index = tokens.len() -1;
+    let last_index = tokens.len() - 1;
 
     for (index, token) in tokens.iter().enumerate() {
         match token {
             Token::Operator { operator, .. } => {
-                if operator == Operator::Divide || operator == Operator::Multiply {
+                if *operator == Operator::Divide || *operator == Operator::Multiply {
                     // we cannot start an expression with a multiply or divide operator
-                    if index == 0  {
+                    if index == 0 {
                         return false;
                     } else {
                         let previous_token = &tokens[index - 1];
                         match previous_token {
-                            Token::Operator {..} => {
+                            Token::Operator { .. } => {
                                 return false;
                             }
-                            Token::Operand {..} => {}
+                            Token::Operand { .. } => {}
                         }
                     }
                 }
 
                 // we cannot end an expression with an operator
-                if index ==  last_index{
+                if index == last_index {
                     return false;
                 }
             }
             Token::Operand { .. } => {
-                let previous_token = &tokens[index - 1];
-                match previous_token {
-                    Token::Operator {..} => {}
-                    Token::Operand {..} => {
-                        return false;
+                if index != 0 {
+                    let previous_token = &tokens[index - 1];
+                    match previous_token {
+                        Token::Operator { .. } => {}
+                        Token::Operand { .. } => {
+                            return false;
+                        }
                     }
                 }
             }
@@ -232,20 +225,70 @@ fn validate_tokens(tokens: &Vec<Token>) -> bool {
     true
 }
 
-fn to_operation(tokens: Vec<Token>) -> Box<dyn Operation> {
-    let first_token = tokens.iter().nth(0).unwrap();
-    match first_token {
-        Token::Operator {operator, index} => {
-            if Operator::Add == *operator || *operator == Operator::Subtract {
-                let second_token = tokens.iter().nth(1).unwrap();
-                match *second_token {
-                    Token::Operator {sec_operator, sec_index} => {
+fn evaluate(tokens: Vec<Token>) -> f32 {
+    let mut initial_pass_result: Vec<Token> = Vec::new();
+    let initial_last_index = tokens.len() - 1;
+    let mut initial_loop_cursor = 0;
 
-                    }
+    while initial_loop_cursor <= initial_last_index {
+        let token = tokens.iter().nth(initial_loop_cursor).unwrap();
+
+        match token {
+            Token::Operator { operator, .. } => {
+                if *operator == Operator::Multiply || *operator == Operator::Divide {
+                    let previous_operand = initial_pass_result.pop().unwrap().to_operand();
+                    let next_operand = tokens
+                        .iter()
+                        .nth(initial_last_index + 1)
+                        .unwrap()
+                        .to_operand();
+
+                    let operation = BinaryOperation {
+                        left_hand: previous_operand,
+                        operator: *operator,
+                        right_hand: next_operand,
+                    };
+
+                    initial_pass_result.push(Token::Operand {
+                        parsed_value: operation.evaluate(),
+                    });
+                    initial_loop_cursor += 2;
+                } else {
+                    initial_pass_result.push(*token);
+                    initial_loop_cursor += 1;
                 }
-            } else {
-                panic!("Unsupported operator");
+            }
+            Token::Operand { .. } => {
+                initial_pass_result.push(*token);
+                initial_loop_cursor += 1;
             }
         }
     }
+
+    let mut result = 0f32;
+
+    let final_last_index = initial_pass_result.len() - 1;
+    let mut final_loop_cursor = 0;
+
+    while final_loop_cursor <= final_last_index {
+        let token = &initial_pass_result[initial_loop_cursor];
+
+        match token {
+            Token::Operator { operator, .. } => {
+                let next_token = &initial_pass_result[initial_loop_cursor + 1];
+                let operation = UnaryOperation {
+                    operator: *operator,
+                    operand: next_token.to_operand(),
+                };
+                result += operation.evaluate();
+                final_loop_cursor += 2;
+            }
+            Token::Operand { parsed_value, .. } => {
+                result += parsed_value;
+                final_loop_cursor += 1;
+            }
+        }
+    }
+
+    result
 }
