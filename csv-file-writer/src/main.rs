275acad55d34a2a162d93380df520279e3584243
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-12-28 18:00:17 +0100

    ft: add in project scaffold

---
+++ csv-file-writer/src/main.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+struct MyData {
+    username: String,
+    first_name: String,
+    last_name: String,
+    identifier: String,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
